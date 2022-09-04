6956b3e3e7ac9f01266ff69050cf07f9d5b783d2
Fathah Cr <abdulfathahkodag@gmail.com>
2022-09-04 15:32:45 +0530

    Typescript Error in Decode JWT

--- functions/decodeJwt.ts
+++ functions/decodeJwt.ts
@@ -3,8 +3,8 @@ import jwt_decode from "jwt-decode";
 
 const decodeJwtAndSetCookie = ()=>{
     if(Cookies.get('token')!=null || Cookies.get('token')!=undefined){
-        const token = Cookies.get('token');
-        var decoded = jwt_decode(token);
+        const token:string = Cookies.get('token') as string;
+        var decoded:any = jwt_decode(token);
         Cookies.set('userid', decoded.id);
         Cookies.set('userstatus', decoded.status);
     }else{
