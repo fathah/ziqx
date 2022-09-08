5668da35c1377c862c899c2efe3cc2b93ab606ad
Fathah Cr <abdulfathahkodag@gmail.com>
2022-09-08 18:00:52 +0530

    Update 404.tsx

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,7 +1,7 @@
 
 const NotFound = () => {
     return (
-        <div className="min-h-screen max-h-screen w-screen flex justify-center items-center px-10 text-center">
+        <div className="min-h-screen max-h-screen w-screen flex justify-center items-center px-10 text-center bg-white">
             <div className="">
            <div className="flex justify-center"> <img src="/imgs/404.gif" alt="404" className="w-8/12" /></div>
             <h1>Oops. The page you are looking for maybe not available right now</h1>
