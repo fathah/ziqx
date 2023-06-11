c088a7305c3a1ff9019203ac5de6f5398e0f6016
Fathah Cr <abdulfathahkodag@gmail.com>
2023-06-11 10:17:45 +0530

    New Products

--- comps/Home/Products.tsx
+++ comps/Home/Products.tsx
@@ -2,19 +2,23 @@
 
 const Products = () => {
     var products = [
-       
-        {name:"Manzil Developers", logo:"manzildev.png"},
-        {name:"Qeats Food Devlivery", logo:"qeats.png"},
-        {name:"Libstackz Library Management", logo:"libstackz.png"},
-        {name:"Tezt Online Tests", logo:"tezt.png"},
-        {name:"Bitpro ERP/POS Software", logo:"bitpro.png"},
-        {name:"Faably SSLC Guide", logo:"faably.png"},
-        {name:"Zaimart Online Store", logo:"zaimart.png"},
-        {name:"Zoozle Online Canvas", logo:"zoozle.png"},
-        {name:"Z Auth", logo:"zauth.png"},
-        {name:"Zinga Secure Messenger", logo:"zinga.png"},
-        {name:"Fabex Framework", logo:"fabex.png"},
-        {name:"ZiDG ZIqx Developers Group", logo:"zidg.png"},
+        {name:"De Studio", logo:"destudio.png",link:"https://destudio.ziqx.in"},
+    {name:"Qeats Food Devlivery", logo:"qeats.png",link:""},
+    {name:"Libstackz Library Management", logo:"libstackz.png",link:"https://libstackz.ziqx.in"},
+    {name:"Manzil Developers", logo:"manzildev.png",link:"https://manzilmedia.net"},
+    {name:"Tezt Online Tests", logo:"tezt.png",link:"https://tezt.ziqx.in"},
+    {name:"Bitpro ERP/POS Software", logo:"bitpro.png",link:""},
+    {name:"Certiption", logo:"certiption.png",link:"https://certiption.com"},
+    {name:"Faably SSLC Guide", logo:"faably.png",link:""},
+    {name:"Zaimart Online Store", logo:"zaimart.png",link:""},
+    {name:"Zoozle Online Canvas", logo:"zoozle.png",link:"https://zoozle.ziqx.in"},
+    {name:"Z Auth", logo:"zauth.png",link:"https://account.ziqx.in"},
+    {name:"Zinga Secure Messenger", logo:"zinga.png",link:"https://thezin.ga"},
+    {name:"Fabex Framework", logo:"fabex.png",link:""},
+    {name:"Zchool", logo:"zchool.png",link:"https://zchool.in"},
+    {name:"ZiDG ZIqx Developers Group", logo:"zidg.png",link:""},
+    {name:"Debaze", logo:"debaze.png",link:"https://debaze.ziqx.in"},
+
     ];
     return (<div className=" py-8 px-4 lg:px-20"
     style={{
@@ -33,6 +37,11 @@ className="flex flex-wrap justify-center items-center">
             <div
             key={prdct.name}
             className="flex text-white font-bold py-5"
+            onClick={()=>{
+                if(prdct.link.length>3){
+                    window.open(prdct.link, "_blank")
+                }
+            }}
             >
             <img src={`/imgs/products/${prdct.logo}`}
             className="mx-4 lg:mx-14 h-16 lg:h-28 product-logo"
