eb1737ba802d94b052993f69433ca466034ab045
Fathah Cr <abdulfathahkodag@gmail.com>
2023-06-16 08:27:12 +0530

    Update MainSection.tsx

--- comps/Home/MainSection.tsx
+++ comps/Home/MainSection.tsx
@@ -22,14 +22,13 @@ const MainSection = () => {
   data-aos="fade-up"
   data-aos-duration="1000"
   >
-    Techology  is easy-peasy
-    with <br />
+    Simplified Tech, Unleashed Potential with <br />
     <span className="bg-yellow-400 inline-block px-2 pb-1 mt-3">Ziqx</span>
     </h1>
     <p className='mt-6 lg:w-8/12 text-xl text-gray-500' data-aos="fade-up"
       data-aos-duration="1200"
     >
-      Presenting digital solutions in a minimal & comprehensive eco-system.
+      Harness the Power of Minimalistic Design and Cutting-Edge Technology for Digital Excellence
     </p>
  <div data-aos="fade-up"
   data-aos-delay="700">
