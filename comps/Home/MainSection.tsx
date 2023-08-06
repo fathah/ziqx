e8808530b522fc68e19b4a35ca81dca663b83e20
Fathah Cr <abdulfathahkodag@gmail.com>
2023-08-06 19:08:35 +0530

    Update MainSection.tsx

--- comps/Home/MainSection.tsx
+++ comps/Home/MainSection.tsx
@@ -22,7 +22,8 @@ const MainSection = () => {
   data-aos="fade-up"
   data-aos-duration="1000"
   >
-    Simplified Tech, Unleashed Potential with <br />
+    Simplified Tech, Unleashed Potential with 
+    <span className="mx-2"></span>
     <span className="bg-yellow-400 inline-block px-2 pb-1 mt-3">Ziqx</span>
     </h1>
     <p className='mt-6 lg:w-8/12 text-xl text-gray-500' data-aos="fade-up"
