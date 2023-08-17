2f5e5aed9b1b54bdab0bc4217805440a026583ea
Fathah Cr <abdulfathahkodag@gmail.com>
2023-08-17 10:50:29 +0530

    Added Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,6 +10,7 @@ import { getConnectionsCloud } from '../functions/connections/get';
 import { isLogged } from '../functions/isLogged';
 import ConnectionsModal from '../comps/user/ConnectionsModal';
 import { ConnectionContextProvider } from '@/context/ConnectionContext';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }: AppProps) {
 
@@ -31,6 +32,17 @@ useEffect(() => {
 
 
   return   <div data-theme="ziqxTheme">
+     <Script src="https://www.googletagmanager.com/gtag/js?id=G-PT1V8MBZDT" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'G-PT1V8MBZDT');
+        `}
+      </Script>
+
     <ConnectionContextProvider>
     <Component {...pageProps} /> <ConnectionsModal/>
     </ConnectionContextProvider>
